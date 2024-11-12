65936edc30a30be417c6cd77ec5ed874b9e7c3c0
antonioarenas9 <antonioarenas9@gmail.com>
2024-11-11 23:14:39 -0500

    carga inicial del proyecto

---
+++ app/src/main/java/com/example/design/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.design
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.linear_layout)setContentView(R.layout.constraint_layout)
+        //setContentView(R.layout.frame_layout)
+        //setContentView(R.layout.constraint_layout)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
